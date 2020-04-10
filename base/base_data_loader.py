corrected model architecture, fixing previously incorrect dimensions. made architecture parameter customizable

base/base_data_loader.py
@@ -1,7 +1,7 @@
 import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
-from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data.sampler import SubsetRandomSampler, SequentialSampler
 
 
 class BaseDataLoader(DataLoader):
@@ -18,7 +18,7 @@ class BaseDataLoader(DataLoader):
         self.sampler, self.valid_sampler = self._split_sampler(self.validation_split)
 
         self.init_kwargs = {
-            'original_data': dataset,
+            'dataset': dataset,
             'batch_size': batch_size,
             'shuffle': self.shuffle,
             'collate_fn': collate_fn,
@@ -33,7 +33,7 @@ class BaseDataLoader(DataLoader):
         idx_full = np.arange(self.n_samples)
 
         np.random.seed(0)
-        np.random.shuffle(idx_full)
+        # np.random.shuffle(idx_full)
 
         if isinstance(split, int):
             assert split > 0
@@ -42,11 +42,15 @@ class BaseDataLoader(DataLoader):
         else:
             len_valid = int(self.n_samples * split)
 
-        valid_idx = idx_full[0:len_valid]
-        train_idx = np.delete(idx_full, np.arange(0, len_valid))
+        # valid_idx = idx_full[0:len_valid]
+        # train_idx = np.delete(idx_full, np.arange(0, len_valid))
+        train_idx = idx_full[:len(idx_full)-len_valid]
+        valid_idx = idx_full[len(idx_full)-len_valid:]
 
-        train_sampler = SubsetRandomSampler(train_idx)
-        valid_sampler = SubsetRandomSampler(valid_idx)
+        # train_sampler = SubsetRandomSampler(train_idx)
+        # valid_sampler = SubsetRandomSampler(valid_idx)
+        train_sampler = SequentialSampler(train_idx)
+        valid_sampler = SequentialSampler(valid_idx)
 
         # turn off shuffle option which is mutually exclusive with sampler
         self.shuffle = False

data_loader/data_loaders.py
@@ -30,11 +30,13 @@ class StockDataset(Dataset):
     def __init__(self, csv_file, window=10):
         self.csv_file = csv_file
         self.df = pd.read_csv(self.csv_file)
+        columns = ['Adj Close', 'Close']
         self.series = torch.tensor(self.df['Adj Close'])
         self.data = []
         self.window = window
         for i in range(0, len(self.series)-self.window):
             self.data.append((self.series[i:i+self.window], self.series[i+self.window]))
+        dummy = 1
 
     def __len__(self):
         return len(self.data)
@@ -47,7 +49,7 @@ class StockDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=False, validation_split=0.0, num_workers=1, training=True):
         self.dataset = StockDataset(data_dir)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 

model/model.py
@@ -26,23 +26,52 @@ class MnistModel(BaseModel):
 class CNNLSTMModel(BaseModel):
     def __init__(self):
         super().__init__()
-        k = 3
-        self.conv1 = nn.Conv1d(1, 1, kernel_size=k) # no padding
-        self.conv2 = nn.Conv1d(1, 1, kernel_size=k)
-        self.rnn1 = nn.RNN(1, 3, 1)
-        self.fc = nn.Linear(3, 1)
+
+        #### Data Dependent ####
+        # input
+        self.input_dim = 1
+        # output
+        self.output_size = 1
+        ########################
+
+        #### Architecture Dependent ####
+        # convolution
+        self.num_conv = 2
+        self.conv_channels = [17, 17]
+        self.kernel_sizes = [3, 3]
+        assert(self.num_conv == len(self.conv_channels)) # ensure right amount of conv channel size is supplied
+
+        self.conv_channels.insert(0, self.input_dim)
+        self.conv_channels = list(zip(self.conv_channels, self.conv_channels[1:])) # convert conv_channels into a list of tuples for easier use
+
+        # rnn
+        self.rnn1_input = self.conv_channels[-1][-1]
+        self.rnn1_hidden = 11
+        self.rnn1_num_layers = 2
+        ################################
+
+        self.convs = nn.ModuleList([nn.Conv1d(in_channels=in_c, out_channels=out_c, kernel_size=k_size) for (in_c, out_c), k_size in zip(self.conv_channels, self.kernel_sizes)])
+        self.rnn1 = nn.LSTM(input_size=self.rnn1_input, hidden_size=self.rnn1_hidden, num_layers=self.rnn1_num_layers)
+        self.fc = nn.Linear(self.rnn1_hidden, self.output_size)
+
         # figure out how to use LSTM later
         # self.lstm1 = nn.LSTM(input_size=1, hidden_size=3, num_layers=2)
         # self.h0 = torch.zeros(2, 1, 3)
         # self.c0 = torch.zeros(2, 1, 3)
 
     def forward(self, x):
-        if x.dim() == 2:
-            x = x.view(x.shape[0], 1, x.shape[1])
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x, _ = self.rnn1(x.view(1, -1, 1))
-        x = self.fc(x[-1, -1])
+        # print("input x.shape: {}".format(x.shape))
+        if x.dim() == 2: # currently only works if x is single series input (can be batched)
+            x = x.view(x.shape[0], 1, x.shape[1]) # batch, channel, sequence_length
+        # print("after view x.shape: {}".format(x.shape))
+        for conv in self.convs:
+            x = F.relu(conv(x))
+            # print("after conv x.shape: {}".format(x.shape))
+        x = x.permute(2, 0, 1) # RNN expects seq_len, batch, channel
+        x, _ = self.rnn1(x) # RNN outputs seq_len, batch, channel
+        # print("after rnn x.shape: {}".format(x.shape))
+        x = self.fc(x[-1, :, :]) # only use the output of the last time step to make our prediction
+        # print("after fc x.shape: {}".format(x.shape))
         # x, _ = self.lstm1(x, (self.h0, self.c0))
         return x
 

scratch.py
@@ -1,20 +1,43 @@
-# from collections import Iterator
-class Count:
-    """Iterator that counts upward forever."""
+import torch
+from torch import nn
+# torch.manual_seed(0)
+# i = 3
+# h = 2
+# l = 1 # irrelavent for the output size
+# batch = 2
+# seq_len = 5
+# rnn = nn.RNN(i, h, l)
+# input = torch.randn(seq_len, batch, i) # seq_len, batch, input channel
+# h0 = torch.randn(l, batch, h) # layer, channel, hidden
+# output, _ = rnn(input, h0)
+# print("first output:")
+# print(output.shape) # batch, channel, hidden dim
+# print(output)
+#
+# print("\noriginal input:")
+# print(input)
+#
+# print("\nmodified input:")
+# input[2][0][0] = 1000.0
+# print(input)
+#
+# print("second output")
+# output, _ = rnn(input, h0)
+# print(output)
+torch.manual_seed(0)
+rnn1_hidden = 11
+output_size = 1
+fc = nn.Linear(rnn1_hidden, output_size)
+x = torch.randn(6, 2, 11)
+print(x.shape)
+print(x)
+o = fc(x[-1, :, :])
+print(o.shape)
+print(o)
 
-    def __init__(self, start=0):
-        self.num = start
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        num = self.num
-        self.num += 1
-        if num == 5:
-            raise TypeError
-        return num
-a = Count(0)
-b = a.__next__()
-print(b)
-print(a.__next__())
+print("modify x")
+x[5][1][0] = 1000.0
+x[5][1][1] = 1000.0
+o = fc(x[-1, :, :])
+print(o.shape)
+print(o)