fixed normalization error

data_loader/data_loaders.py
@@ -90,9 +90,9 @@ class StockDataLoader(BaseDataLoader):
         np_matrix = np.stack(np_array_list, axis=1)
         self.transformer = StandardScaler()
         np_matrix_normalized = self.transformer.fit_transform(np_matrix)
-
         # save the mean and variance to file
-        np.savez("norm_para", mean=self.transformer.mean_, var=self.transformer.var_)
+
+        np.savez("norm_para", mean=self.transformer.mean_, std=np.sqrt(self.transformer.var_))
 
         np_matrix_original = self.transformer.inverse_transform(np_matrix_normalized)
         torch_matrix = torch.tensor(np_matrix_normalized, dtype=torch.float).t()

model/metric.py
@@ -33,10 +33,10 @@ def regression_binary_pred(output, target):
         # apply the inverse transform in here
         # load the transformation params from the saved file
         with np.load('norm_para.npz') as para:
-            mean, var = [para[i] for i in ('mean', 'var')]
+            mean, std = [para[i] for i in ('mean', 'std')]
 
-        output_np = (output_np*var)+mean
-        target_np = (target_np*var)+mean
+        output_np = (output_np*std)+mean
+        target_np = (target_np*std)+mean
 
         sign_match = np.sign(output_np) == np.sign(target_np)
         sign_match_percent = np.sum(sign_match)/np.size(sign_match)