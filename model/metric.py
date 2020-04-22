fixed F-1 score, precision, recall and matrix

model/metric.py
@@ -72,13 +72,15 @@ def f1_score(output, target):
         FP = np.sum((np.sign(target_np) - np.sign(output_np)) == -2)
         FN = np.sum((np.sign(target_np) - np.sign(output_np)) == 2)
 
-        precision = TP / (TP + FP)
-        recall = TP / (TP + FN)
-        F_1_score = 2 * precision * recall / (precision + recall)
+        # precision = TP / (TP + FP)
+        # recall = TP / (TP + FN)
+        # F_1_score = 2 * precision * recall / (precision + recall)
 
         # np.savez("results.npz", F_1_score=F_1_score)
 
-    return F_1_score
+    # this function is useless now
+
+    return 0
 
 
 def MAPE(output,target):
@@ -87,8 +89,8 @@ def MAPE(output,target):
         output_np = output.cpu().numpy()
         target_np = target.cpu().numpy()
 
-        output_np = unnormalize(output_np).flatten()
-        target_np = unnormalize(target_np).flatten()
+        output_np = unnormalize(output_np)
+        target_np = unnormalize(target_np)
 
         N = output_np.shape[0]
 

train.py
@@ -52,7 +52,7 @@ def main(config,config_f):
 #     save result to JSON
 
     with np.load('results.npz') as result:
-        mse, sharpe, reg_binary_pred, F_1_score, MAPE, conf_mtx = [result[i] for i in ('mse_loss', 'regression_sharpe', 'regression_binary_pred', 'F_1_score', 'MAPE','conf_mtx')]
+        mse, sharpe, reg_binary_pred, F_1_score,precision,recall, MAPE, conf_mtx = [result[i] for i in ('mse_loss', 'regression_sharpe', 'regression_binary_pred', 'F_1_score','precision','recall', 'MAPE','conf_mtx')]
 
     if os.path.exists("results.npz"):
         os.remove("results.npz")
@@ -66,6 +66,8 @@ def main(config,config_f):
     data["results"]["f-1 score"] = np.sum(F_1_score)
     data["results"]["mape"] = np.sum(MAPE)
     data["results"]["confusion mtx"] = conf_mtx.tolist()
+    data["results"]["precision"] = np.sum(precision)
+    data["results"]["recall"] = np.sum(recall)
 
     with open(config_f, "w") as f_object:
         json.dump(data, f_object,indent=4)
@@ -79,10 +81,13 @@ def save_to_excel():
     target_columns = []
     regression_binary_pred = []
     F_1_score = []
+    precision=[]
+    recall = []
     mse = []
     sharpe = []
     mape = []
     conf_mtx = []
+
 
     for fname in glob.glob('data_loader/configs/*.json'):
         with open(fname, "r") as f_object:
@@ -95,13 +100,15 @@ def save_to_excel():
             target_columns.append(data["data_loader"]["args"]["target_columns"])
             regression_binary_pred.append(data["results"]["accuracy"])
             F_1_score.append(data["results"]["f-1 score"])
+            precision.append(data["results"]["precision"])
+            recall.append(data["results"]["recall"])
             mse.append(data["results"]["mse"])
             sharpe.append(data["results"]["sharpe"])
             mape.append(data["results"]["mape"])
             conf_mtx.append(np.array_str(np.array(data["results"]["confusion mtx"])))
 
-    df = pd.DataFrame(np.array([name_list,batch_size,context_win,input_columns,target_columns,regression_binary_pred,F_1_score,mse,sharpe,mape,conf_mtx]).T,
-                      columns=["file names","batch size","context window","input columns","target_columns","regression_binary_pred","F_1_score","mse","sharpe","mape","confusion matrix"])
+    df = pd.DataFrame(np.array([name_list,batch_size,context_win,input_columns,target_columns,regression_binary_pred,F_1_score,precision,recall,mse,sharpe,mape,conf_mtx]).T,
+                      columns=["file names","batch size","context window","input columns","target_columns","regression_binary_pred","F_1_score","precision","recall","mse","sharpe","mape","confusion matrix"])
 
     print(df)
 

trainer/trainer.py
@@ -78,19 +78,45 @@ class Trainer(BaseTrainer):
 
         if self.do_validation:
             val_log, conf_matrix = self._valid_epoch(epoch)
-            val_log["confusion matrix"] = conf_matrix
+
+            # Have to calcluate F-1 score in here
+            TP = conf_matrix[0][1]
+            TN = conf_matrix[0][0]
+            FP = conf_matrix[1][1]
+            FN = conf_matrix[1][0]
+
+            precision = TP / (TP + FP)
+            recall = TP / (TP + FN)
+            f_1_score = 2 * precision * recall / (precision + recall)
+
+
+
+            val_log['f1_score'] = f_1_score
+            val_log["confusion_matrix"] = conf_matrix
             self.trader.plot_ret()
             buy_and_hold_sharpe, regression_sharpe = self.trader.get_sharpe()
             log.update(**{'val_'+k : v for k, v in val_log.items()})
+
             if os.path.exists("results.npz"):
                 with np.load("results.npz") as result:
-                    mse, sharpe, reg_binary_pred, F_1_score, MAPE = [result[i] for i in ('mse_loss', 'regression_sharpe', 'regression_binary_pred', 'F_1_score','MAPE')]
+                    mse, sharpe, reg_binary_pred, F_1_score, old_precision, old_recall, MAPE = [result[i] for i in ('mse_loss', 'regression_sharpe', 'regression_binary_pred', 'F_1_score','precision','recall','MAPE')]
 
-                np.savez("results.npz", mse_loss=min(mse, val_log["loss"]), regression_sharpe=max(sharpe, regression_sharpe),
-                         regression_binary_pred=max(reg_binary_pred, val_log["regression_binary_pred"]),MAPE = min(MAPE,val_log["MAPE"]),
-                         F_1_score=max(F_1_score, val_log["f1_score"]),conf_mtx = conf_matrix)
+                np.savez("results.npz", mse_loss=min(mse, val_log["loss"]),
+                         regression_sharpe=max(sharpe, regression_sharpe),
+                         regression_binary_pred=max(reg_binary_pred, val_log["regression_binary_pred"]),
+                         MAPE = min(MAPE,val_log["MAPE"]),
+                         F_1_score=max(F_1_score, f_1_score),
+                         precision=max(old_precision, precision),
+                         recall=max(old_recall, recall),
+                         conf_mtx = conf_matrix)
             else:
-                np.savez("results.npz", mse_loss=val_log["loss"], regression_sharpe=regression_sharpe, regression_binary_pred=val_log["regression_binary_pred"], F_1_score=val_log["f1_score"],MAPE = val_log["MAPE"],conf_mtx = conf_matrix)
+                np.savez("results.npz", mse_loss=val_log["loss"],
+                         regression_sharpe=regression_sharpe,
+                         regression_binary_pred=val_log["regression_binary_pred"],
+                         F_1_score=f_1_score,
+                         precision = precision,
+                         recall = recall,
+                         MAPE = val_log["MAPE"],conf_mtx = conf_matrix)
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
         return log

utils/util.py
@@ -30,7 +30,7 @@ class MetricTracker:
     def __init__(self, *keys, writer=None):
         self.writer = writer
         self._data = pd.DataFrame(index=keys, columns=['total', 'counts', 'average'])
-        self.conf_mtx = None
+        self.conf_mtx = np.array([[0,0],[0,0]])
         self.reset()
         
     def reset(self):
@@ -40,12 +40,14 @@ class MetricTracker:
     def update(self, key, value, n=1):
         if self.writer is not None:
             self.writer.add_scalar(key, value)
-        if type(value) is not np.ndarray:
+
+        if key =='confusion_matrix':
+            self.conf_mtx+=value
+        else:
             self._data.total[key] += value * n
             self._data.counts[key] += n
             self._data.average[key] = self._data.total[key] / self._data.counts[key]
-        else:
-            self.conf_mtx = value
+
 
     def avg(self, key):
         return self._data.average[key]
@@ -55,3 +57,4 @@ class MetricTracker:
 
     def result(self):
         return dict(self._data.average)
+